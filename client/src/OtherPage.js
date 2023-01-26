018866b0b508bea3122b0b12c1fafb203f808820
SEBAA300 <sebaa.13.06@gmail.com>
2023-01-26 23:11:17 +0100

    add files

---
+++ client/src/OtherPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+export default () => {
+    return (
+        <div>
+            <p>Multifibo</p>
+            <p>Sebastian Alchimowicz</p>
+            <p>Grupa 2.1</p>
+            <Link to="/">Back to home</Link>
+        </div>
+    );
+};
